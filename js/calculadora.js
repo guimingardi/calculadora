6a30339170c509237c2ac9a83cf252c494a64d5a
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-08-14 14:45:40 -0300

    Inicia codigo de implementacao de operacoes com o teclado

--- js/calculadora.js
+++ js/calculadora.js
@@ -14,73 +14,59 @@ let clear = document.querySelector('.one-one').addEventListener('click', um_por_
 let dot = document.querySelector('.dot').addEventListener('click', ponto)
 document.addEventListener('keydown', digitado)
 
-let digitou
+document.addEventListener('keydown', resultado)
 
+let digitou
+let operador
+let valor_um
+let valor_dois
+let apaga_um
+let contador = 0
+let tc_ops
 
 function digitado() {
-    digitou = event.keyCode
-    if (digitou == 49) {
-        tela.innerText += 1
+    digitou = event.key
+    console.log(digitou)
+
+    if (digitou == 'Backspace') {
+        tela.innerText = tela.innerText.slice(0, -1)
+    } else if (digitou >= 0 || digitou <= 9) {
+        tela.innerText += digitou
     }
-    else if (digitou == 50) {
-        tela.innerText += 2
+    else if (digitou == '.') {
+        if (contador > 1) {
+            return
+        }
+        tela.innerText += digitou
+        ponto()
+        contador++
     }
-    else if (digitou == 51) {
-        tela.innerText += 3
+}
+function resultado() {
+    tc_ops = event.key
+    if (tc_ops == '+') {
+        valor_um = Number(tela.innerHTML)
+        tela.innerText = ''
+        result = valor_um + valor_dois
     }
-    else if (digitou == 52) {
-        tela.innerText += 4
-    }
-    else if (digitou == 53) {
-        tela.innerText += 5
-    }
-    else if (digitou == 54) {
-        tela.innerText += 6
-    }
-    else if (digitou == 55) {
-        tela.innerText += 7
-    }
-    else if (digitou == 56) {
-        tela.innerText += 8
-    }
-    else if (digitou == 57) {
-        tela.innerText += 9
-    }
-    else if (digitou == 48) {
-        tela.innerText += 0
-    }
-    else if (digitou == 48) {
-        tela.innerText += 0
-    } else if (digitou == 8) {
-        um_por_vez()
-    }
-    else if (digitou == 27) {
-        zerar()
-    }
-    else if (digitou == 187){
-        igual()
+    else if (digitou == '=') {
+        valor_dois = Number(tela.innerText)
+        valor_um = result
+        result = tela.innerText
+        console.log(result)
     }
 }
 
-let operador
-let valor_um
-let apaga_um
-let contador = 0
-
 function operacao() {
     valor_um = Number(tela.innerHTML)
-    console.log(`Tenho valor1 ${valor_um}`)
     tela.innerText = ''
     operador = event.target.innerText
     contador = 0
 }
 
 function igual() {
-    digitou = event.keyCode
-    let valor_dois = Number(tela.innerText)
-    // console.log(`Tem valor dois = ${valor_dois}`)
+    valor_dois = Number(tela.innerText)
     tela.innerText = ''
-    console.log(digitou)
     if (operador == '/') {
         result = valor_um / valor_dois
         console.log(digitou)
@@ -100,12 +86,10 @@ function igual() {
 
     valor_um = result
     tela.innerText = result
-    // console.log(`Tem valor1 = ${valor_um}`)
 }
 
 function um_por_vez() {
     tela.innerText = tela.innerText.slice(0, -1)
-    // preciso zerar o contador quando o ponto for apagado
 }
 
 function zerar() {
@@ -116,14 +100,56 @@ function zerar() {
 }
 
 function ponto() {
+    // if (contador > 1) {
+
+    // }
     contador++
-    if (contador > 1) {
-        tela.innerText = 'Error'
-    }
+
     if (tela.innerText == '.') {
         tela.innerText = '0.'
     }
 }
 
-
+    // console.log(event.keyCode)
+    // if (digitou == 49) {
+    //     tela.innerText += 1
+    // }
+    // else if (digitou == 50) {
+    //     tela.innerText += 2
+    // }
+    // else if (digitou == 51) {
+    //     tela.innerText += 3
+    // }
+    // else if (digitou == 52) {
+    //     tela.innerText += 4
+    // }
+    // else if (digitou == 53) {
+    //     tela.innerText += 5
+    // }
+    // else if (digitou == 54) {
+    //     tela.innerText += 6
+    // }
+    // else if (digitou == 55) {
+    //     tela.innerText += 7
+    // }
+    // else if (digitou == 56) {
+    //     tela.innerText += 8
+    // }
+    // else if (digitou == 57) {
+    //     tela.innerText += 9
+    // }
+    // else if (digitou == 48) {
+    //     tela.innerText += 0
+    // }
+    // else if (digitou == 48) {
+    //     tela.innerText += 0
+    // } else if (digitou == 8) {
+    //     um_por_vez()
+    // }
+    // else if (digitou == 27) {
+    //     zerar()
+    // }
+    // else if (digitou == 187){
+    //     igual()
+    // }
 
